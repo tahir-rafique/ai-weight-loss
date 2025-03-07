9667cd609ac50b1498aed023060faaea16fdc21a
itstahirrafique <hello.tahirrafique@gmail.com>
2025-03-07 12:28:25 +0500

    initalized a new repo for this project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-green-300'>Hello ai weight loose</h1>
+      <h3 className='bg-green-300'>Hello world</h3>
+      
+    </div>
+  )
+}
+
+export default App
