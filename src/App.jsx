fdb32c1eb26afb3341a075bf98d6e9b3580f02ab
itstahirrafique <hello.tahirrafique@gmail.com>
2025-03-07 15:25:48 +0500

    rest styling for all elemnets

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1 >h1</h1>
-      <h2>h2</h2>
-      <h3>h3</h3>
-      <h4>h4</h4>
-      <h5>h5</h5>
-      <h6>h6</h6>
-    </div>
+    <>
+
+      <h1 className='bg-red-300 '>Hello</h1>
+    </>
   )
 }
 
