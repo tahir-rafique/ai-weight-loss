764a433135060a21cd151e88bcb457c00c8a4a5e
itstahirrafique <hello.tahirrafique@gmail.com>
2025-03-07 14:27:04 +0500

    tailwindConfig updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './styles/main.scss'
 import './styles/tailwindConfig.css'
+import './styles/main.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
