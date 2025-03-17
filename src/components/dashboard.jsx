51f61e2004331dcaea81baa957451523d0425bcf
itstahirrafique <hello.tahirrafique@gmail.com>
2025-03-17 13:27:24 +0500

    dashboard updated

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,196 +1,216 @@
 import React from 'react'
-import { GoStopwatch } from "react-icons/go";
-import { IoMdNotificationsOutline } from "react-icons/io";
-import { RxAvatar } from "react-icons/rx";
-import piechart from '../../assets/icons/piechart.svg'
 import { TbSettingsCode } from "react-icons/tb";
 import { FaRegCheckCircle } from "react-icons/fa";
+import dashClock from '../assets/icons/dashboard/dash-clock.svg'
+import notfication from '../assets/icons/dashboard/notfication.svg'
+import userImg from '../assets/icons/dashboard/userImg.svg'
+import arrowCircleRight from '../assets/icons/dashboard/arrow-circle-right.svg'
+import arrowCircleLeft from '../assets/icons/dashboard/arrow-circle-left.svg'
+import threeDotIcon from '../assets/icons/dashboard/three-dot-icon.svg'
+import tickGreen from '../assets/icons/dashboard/tick-green.svg'
+import statChart from '../assets/icons/dashboard/pie-chart-stat.svg'
+
+
+
 
 const Dashborad = () => {
   return (
-    <div className='bg-[var(--color-bg)] relative z-100 rounded-[8px] px-5 py-4 '>
-
+    <div className='bg-[var(--color-bg-white)] rounded-[8px]  h-full w-full px-5 py-8 flex flex-col gap-8'>
       {/* dashboard-top */}
-
-      <div className="dashboard-top flex justify-between pb-5">
-        <h4 className='text-[var(--color-btn)] font-bold'>Dashboard</h4>
-        <div className='rounded-[30px] shadow-md flex gap-2 px-4 py-2 items-center justify-center'>
-          <GoStopwatch size={30} />
-          <IoMdNotificationsOutline size={30} />
-          <RxAvatar size={30} />
+      <div className="dashboard-top flex justify-between">
+        <h4 className='text-[var(--color-btn)] font-[750] font-secondary'>Dashboard</h4>
+        <div className='dash-icons rounded-[30px] shadow-xs flex gap-2 px-4 py-2 items-center justify-between'>
+          <div className='clock cursor-pointer'>
+            <img src={dashClock} alt="" />
+            <span className='text-xs'>Reminder</span>
+          </div>
+          <img src={notfication} alt="" className=' cursor-pointer ' />
+          <img src={userImg} alt="" className='cursor-pointer' />
         </div>
       </div>
 
+      {/* Dash-bottom */}
+      <div className="dashboard-bottom flex flex-col gap-34">
+        <div className="shadow-[#fcc378] shadow-xs  border-[var(--color-primary)] border-b-[10px] p-5 pl-[40px] rounded-[8px] flex flex-col gap-y-[26px]">
 
-      {/* dashboard-main */}
-      <div className="dashboard-main flex flex-col gap-4">
-
-        {/* Dash-center */}
-        <div className="dashboard-center shadow-md border-[1px] border-[var(--color-primary)] border-b-[10px] px-10 py-2 myrounded">
-
-          <div className='flex justify-between gap-2 '>
-            <h4 className='text-[var(--color-btn)] font-semibold'>Today's <span className='text-[var(--color-dark)]'>Goals</span> to Achieve</h4>
-
-            <div className='flex items-center gap-2'>
-              <p className='text-xs font-semibold'>Nov (2024)</p>
-              <div className='flex gap-2 items-center'>
-                <button>--</button>
-                <button>--</button>
+          <div className='gaols-section flex flex-col gap-y-6'>
+            <div className='flex justify-between'>
+              <h4 className='text-[var(--color-primary)] font-semibold font-secondary'>
+                Today's<span className='text-[var(--color-dark)]'> Goals </span>to Achieve
+              </h4>
+              <div className='flex items-center gap-1'>
+                <p className='text-small font-semibold font-secondary'>Nov (2024)</p>
+                <button > <img src={arrowCircleLeft} alt="" /></button>
+                <button> <img src={arrowCircleRight} alt="" /></button>
               </div>
             </div>
-          </div>
 
-          {/* date */}
-          <div className='w-full flex justify-between pt-4'>
-            <div className='w-fit'>
-              <p>Mon</p>
-              <p className='text-[var(--color-btn)] text-center'>1</p>
-            </div>
+            <div className='flex flex-col gap-y-[23px]'>
+              {/* days & date */}
+              <div className='w-[95%] flex justify-between items-center'>
 
-
-            <div className='w-fit'>
-              <p>Tue</p>
-              <p className='text-[var(--color-btn)] text-center'>2</p>
-            </div>
-
-            <div className='w-fit'>
-              <p>Wed</p>
-              <p className='text-[var(--color-btn)] text-center'>3</p>
-            </div>
-
-            <div className='w-fit'>
-              <p>Thu</p>
-              <p className='text-[var(--color-btn)] text-center'>4</p>
-            </div>
-
-            <div className='w-fit'>
-              <p>Fri</p>
-              <p className='text-[var(--color-btn)] text-center'>5</p>
-            </div>
-
-
-            <div className='w-fit'>
-              <p>Sat</p>
-              <p className='text-[var(--color-btn)] text-center'>6</p>
-            </div>
-
-            <div className='w-fit'>
-              <p>Sun</p>
-              <p className='text-[var(--color-btn)] text-center'>7</p>
-            </div>
-
-            <div className='w-fit'>
-              <p>Mon</p>
-              <p className='text-[var(--color-btn)] text-center'>8</p>
-            </div>
-
-
-            <div className='w-fit'>
-              <p>Tue</p>
-              <p className='text-[var(--color-btn)] text-center'>9</p>
-            </div>
-
-
-            <div className='w-fit'>
-              <p>Wed</p>
-              <p className='text-[var(--color-btn)] text-center'>10</p>
-            </div>
-
-
-            <div className='w-fit'>
-              <p>Thu</p>
-              <p className='text-[var(--color-btn)] text-center'>11</p>
-            </div>
-
-
-            <div className='w-fit'>
-              <p>Fri</p>
-              <p className='text-[var(--color-btn)] text-center'>12</p>
-            </div>
-
-            <div className='w-fit'>
-              <p>Sat</p>
-              <p className='text-[var(--color-btn)] text-center'>13</p>
-            </div>
-
-            <div className='w-fit'>
-              <p>Sun</p>
-              <p className='text-[var(--color-btn)] text-center'>14</p>
-            </div>
-
-            <div className='w-fit pr-12'>
-              <p>Mon</p>
-              <p className='text-[var(--color-btn)] text-center'>15</p>
-            </div>
-
-
-
-          </div>
-
-          {/* goal completed */}
-          <div className='py-8 flex flex-col gap-4'>
-            <div className='w-full flex gap-2'>
-
-              <div className='w-[100px] flex items-center'>
-                <h6 className=' text-[var(--color-dark-light)]'>08:30 Am</h6>
-              </div>
-
-              <div className='w-[calc(100%-100px)] bg-[var(--color-bg)] rounded-[8px] py-3 px-5 flex flex-col gap-4'>
-                <div className='flex justify-between items-top gap-2'>
-                  <h5 className='font-bold'>Headache Release</h5>
-                  <div className='text-2xl font-black rounded-full'>...</div>
-
+                <div className='w-fit'>
+                  <p>Mon</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>1</p>
                 </div>
 
-                {/* 1 one */}
-                <div className='flex justify-between items-center gap-2'>
-                  <p><span className='text-[var(--color-heading)]'>Duration:</span> 07 days</p>
-                  <div className='flex items-center gap-2 bg-[var(--sec-bg)] px-2 py-1 rounded-[8px]'>
-                    <FaRegCheckCircle className='text-[var(--text-sec)]' />
-                    <p className='text-large font-bold text-[var(--text-sec)]'> Goal completed</p>
+                <div className='w-fit'>
+                  <p>Tue</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>2</p>
+                </div>
+
+
+                <div className='w-fit'>
+                  <p>Wed</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>3</p>
+                </div>
+
+
+                <div className='w-fit'>
+                  <p>Thu</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>4</p>
+                </div>
+
+                <div className='w-fit bg-[var(--color-btn)] rounded-[27px] px-4 py-2 text-white flex flex-col justify-center items-center'>
+                  <p>Fri</p>
+                  <p className='text-center font-bold text-white'>5</p>
+                </div>
+
+
+                <div className='w-fit'>
+                  <p>Sat</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>6</p>
+                </div>
+
+
+                <div className='w-fit'>
+                  <p>Sun</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>7</p>
+                </div>
+
+                <div className='w-fit'>
+                  <p>Mon</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>8</p>
+                </div>
+
+                <div className='w-fit'>
+                  <p>Tue</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>9</p>
+                </div>
+
+
+                <div className='w-fit'>
+                  <p>Wed</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>10</p>
+                </div>
+
+
+                <div className='w-fit'>
+                  <p>Thu</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>11</p>
+                </div>
+
+                <div className='w-fit'>
+                  <p>Fri</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>12</p>
+                </div>
+
+
+                <div className='w-fit'>
+                  <p>Sat</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>13</p>
+                </div>
+
+
+                <div className='w-fit'>
+                  <p>Sun</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>14</p>
+                </div>
+
+                <div className='w-fit'>
+                  <p>Mon</p>
+                  <p className='text-[var(--color-btn)] text-center font-bold'>15</p>
+                </div>
+
+
+              </div>
+
+              {/* goal completed */}
+              <div className=' flex flex-col gap-y-[15px] '>
+
+                <div className='goal-complete flex gap-10'>
+                  {/* div-1 */}
+                  <div className=' flex items-center w-30'>
+                    <h6 className=' text-[var(--color-dark-light)] font-semibold '>08:30 Am</h6>
                   </div>
-                </div>
-              </div>
-            </div>
+                  {/* div-2 */}
+                  <div className='bg-[var(--color-bg)] rounded-[8px] flex flex-col gap-2 w-full p-5 '>
+                    <div className='flex justify-between items-center '>
+                      <h5 className='font-bold'>Headache Release</h5>
+                      <div className='text-2xl font-black rounded-full'>
+                        <img src={threeDotIcon} alt="" />
+                      </div>
+                    </div>
 
-            <div className='w-full flex gap-2'>
+                    <div className='flex justify-between items-center '>
+                      <p><span className='text-[var(--color-heading)]'>Duration:</span> 07 days</p>
 
-              <div className='w-[100px] flex items-center'>
-                <h6 className=' text-[var(--color-dark-light)]'>08:30 Am</h6>
-              </div>
+                      <div className='flex items-center gap-2 bg-[var(--sec-bg)] px-2 py-1 rounded-[8px]'>
+                        <img src={tickGreen} alt="goal-completed-icon" />
+                        <p className='text-large font-bold text-[var(--text-sec)]'> Goal completed</p>
+                      </div>
 
-              <div className='w-[calc(100%-100px)] bg-[var(--color-bg)] rounded-[8px] py-3 px-5 flex flex-col gap-4'>
-                <div className='flex justify-between items-center gap-2'>
-                  <h5 className='font-bold'>Headache Release</h5>
-                  <div className='text-2xl font-black rounded-full'>...</div>
-                </div>
+                    </div>
 
-                {/* 2 div */}
-                <div className='flex justify-between items-center gap-2'>
-                  <p><span className='text-[var(--color-heading)]'>Duration:</span> 07 days</p>
-                  <div className='flex items-center gap-2 bg-[var(--sec-bg)] px-2 py-1 rounded-[8px]'>
-                    <FaRegCheckCircle className='text-[var(--text-sec)]' />
-                    <p className='text-large font-bold text-[var(--text-sec)]'> Goal completed</p>
                   </div>
+
                 </div>
+
+
+                <div className='goal-complete flex gap-10'>
+                  {/* div-1 */}
+                  <div className=' flex items-center w-30'>
+                    <h6 className=' text-[var(--color-dark-light)] font-semibold '>08:30 Am</h6>
+                  </div>
+                  {/* div-2 */}
+                  <div className='bg-[var(--color-bg)] rounded-[8px] flex flex-col gap-2 w-full p-5 '>
+                    <div className='flex justify-between items-center '>
+                      <h5 className='font-bold'>Headache Release</h5>
+                      <div className='text-2xl font-black rounded-full'>
+                        <img src={threeDotIcon} alt="" />
+                      </div>
+                    </div>
+
+                    <div className='flex justify-between items-center '>
+                      <p><span className='text-[var(--color-heading)]'>Duration:</span> 07 days</p>
+
+                      <div className='flex items-center gap-2 bg-[var(--sec-bg)] px-2 py-1 rounded-[8px]'>
+                        <img src={tickGreen} alt="goal-completed-icon" />
+                        <p className='text-large font-bold text-[var(--text-sec)]'> Goal completed</p>
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </div>
+
 
               </div>
             </div>
-
           </div>
-
         </div>
 
-        {/* Dash-bottom */}
-        <div className="dashboard-bottom shadow-md border-[1px] border-[var(--color-primary)] border-b-[10px] px-10 py-5 myrounded flex items-end gap-4">
-          <div className='bottom-left'>
-            <h4 className='text-[var(--color-btn)]'>Statistics</h4>
-            <img src={piechart} alt="no" className='size-50' />
+
+        <div className="statistic-section shadow-[#fcc378] shadow-xs  border-[var(--color-primary)] border-b-[10px] p-5 myrounded flex items-end gap-4">
+          <div className='bottom-left flex flex-col gap-1 bg-red-600'>
+            <h4 className='text-[var(--color-btn)] font-semibold font-secondary'>Statistics</h4>
+            <img src={statChart} alt="" />
           </div>
 
-          <div className='bottom-right flex justify-between w-full px-10'>
+          <div className='bottom-right flex justify-between w-full  bg-green-500'>
 
-            <div className='shadow-xs shadow-[#FFC6AF] border-[var(--color-primary)] border-b-[10px] px-10 py-14 myrounded flex items-center gap-4'>
+            <div className='shadow-xs shadow-[#FFC6AF] border-[var(--color-primary)] border-b-[10px]  myrounded flex items-center gap-4'>
               <div>
                 <TbSettingsCode size={80}
                   className='text-[var(--color-primary' />
@@ -203,7 +223,7 @@ const Dashborad = () => {
 
             </div>
 
-            <div className='shadow-xs shadow-[#FFC6AF] border-[var(--color-primary)] border-b-[10px] px-10 py-14 myrounded flex items-center gap-4'>
+            <div className='shadow-xs shadow-[#FFC6AF] border-[var(--color-primary)] border-b-[10px]  myrounded flex items-center gap-4'>
               <div>
                 <TbSettingsCode size={80} className='text-[var(--color-uniq)] ' />
               </div>
@@ -217,7 +237,7 @@ const Dashborad = () => {
 
 
 
-            <div className='shadow-xs shadow-[#FFC6AF] border-[var(--color-primary)] border-b-[10px] px-10 py-14 myrounded flex items-center gap-4'>
+            <div className='shadow-xs shadow-[#FFC6AF] border-[var(--color-primary)] border-b-[10px]  myrounded flex items-center gap-4'>
               <div>
                 <TbSettingsCode size={80} className='text-[var(--color-heading)] ' />
               </div>
@@ -233,6 +253,8 @@ const Dashborad = () => {
 
           </div>
         </div>
+
+
       </div>
     </div>
 
@@ -240,4 +262,4 @@ const Dashborad = () => {
   )
 }
 
-export default Dashborad
+export default Dashborad;
--- src/pages/dashboard/dashboradPage.jsx
+++ src/pages/dashboard/dashboradPage.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import SideBar from '../../components/sideBar'
+import mdlogo from '../../assets/icons/mdlogo.webp'
+import Dashborad from './../../components/dashboard';
 
 
 const DashboradPage = () => {
     return (
-        <div className='p-5 bg-[var(--color-primary)] h-screen w-screen flex'>
+        <div className='p-5 bg-[var(--color-bg)] h-screen w-screen flex'>
             {/* sidebar */}
-            <div className="sidebar overflow-y-auto w-[20%] h-full">
+            <div className="sidebar overflow-y-auto w-[260px] h-full">
                 <div>
-                    <h1 className='h-[60px]'>fafdsafasfd</h1>
+                    <img src={mdlogo} alt="" className='h-[60px]' />
                 </div>
-                <div className=' bg-white rounded-tl-lg rounded-bl-[8px]  h-[calc(100%_-_60px)]'>
+                <div className='rounded-tl-lg rounded-bl-[8px]  h-[calc(100%_-_60px)]'>
                     <SideBar />
                 </div>
             </div>
 
             {/* main */}
-            <div className="dashboard bg-white rounded-e-[8px] rounded-tl-[8px] w-[80%] ">
-                kjhbvcxdrtyuiuytrewqasdfgbnuy6rdc
+            <div className="rounded-e-[8px] rounded-tl-[8px] w-full ">
+                <Dashborad />
             </div>
         </div>
     )
