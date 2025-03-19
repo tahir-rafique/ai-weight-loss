e699aec04d43de67df4e99a7a63b2602f9c9b097
itstahirrafique <hello.tahirrafique@gmail.com>
2025-03-19 10:15:14 +0500

    sidebar links styling updated

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -23,41 +23,58 @@ const SideBar = () => {
         <div className='sidebar bg-[var(--color-bg-white)] flex flex-col rounded-tl-[8px] rounded-bl-[8px]  h-full w-full'>
 
             <div className='flex flex-col justify-between h-full w-full p-3'>
-                <div className='sidebar-links flex flex-col gap-y-2  text-[var(--color-dark-light)]'>
-                    <NavLink to='/dashboard-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3' >
+                <div className='sidebar-links flex flex-col gap-y-2  text-[var(--color-dark-light)] pt-4'>
+                    <NavLink
+                        to='/dashboard-page'
+                        className='navlink flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'
+                    >
                         <img src={dashboard} alt="" />
                         <p className='font-medium'>  Dashboard  </p>
                     </NavLink>
 
                     {/* ai-chat */}
-                    <NavLink to='conversation' className={` flex gap-2 items-center hover:shadow-md rounded-[8px] px-4 py-3`}>
+                    <NavLink
+                        to='/conversation'
+                        className="navlink flex gap-2 items-center hover:shadow-md rounded-[8px] px-4 py-3"
+                    >
                         <img src={aichat} alt="" />
                         <p className='font-medium'>  AI Health Coach  </p>
                     </NavLink>
 
                     {/* Goals */}
 
-                    <NavLink to='/goals-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
+                    <NavLink
+                        to='/goals-page'
+                        className='navlink flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'
+                    >
                         <img src={goal} alt="" />
                         <p className='font-medium'>  Goals  </p>
                     </NavLink>
 
                     {/* Community */}
-                    <NavLink to='/community-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
+                    <NavLink
+                        to='/community-page'
+                        className='navlink flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'
+                    >
                         <img src={community} alt="" />
                         <p className='font-medium'>  Community  </p>
                     </NavLink>
 
                     {/* Settings */}
-                    <NavLink to='/settings-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
+                    <NavLink
+                        to='/settings-page'
+                        className='navlink flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'
+                    >
                         <img src={setting} alt="" />
                         <p className='font-medium'>  Settings  </p>
                     </NavLink>
                 </div>
 
                 <div>
-                    <Btn title='Logout' className='border border-[var(--color-btn)] font-medium text-center rounded-[8px] py-2 text-[var(--color-btn)]'
+                    <Btn
+                        title='Logout'
                         onClick={() => { navigate('/') }}
+                        className='border border-[var(--color-btn)] font-medium text-center rounded-[8px] py-2 text-[var(--color-btn)]'
                     />
                 </div>
             </div>
