88764b00ea43d7fb28d8f51b8c250aebb7ec158b
itstahirrafique <hello.tahirrafique@gmail.com>
2025-03-19 09:44:16 +0500

    notfication toggel button modified

--- src/components/shared/SwitchNotfication.jsx
+++ src/components/shared/SwitchNotfication.jsx
@@ -1,21 +1,17 @@
 import { useState } from "react";
 
-import classNames from "classnames";
-// class name is imported as classNames from npm, ya ek NPM ka package hai jo class name ko manage karne me help karta hai Condiction kee base pe class name add/remove karne me help karta hai.
 
 const SwitchNotfication = () => {
-  const [isSelected, setisSelected] = useState(false)
+  const [isSelected, setIsSelected] = useState(false)
 
   return (
-    <div
-      onClick={() => setisSelected(!isSelected)}
-      className={classNames("cursor-pointer flex items-center w-10 h-5 bg-[var(--color-btn)] border border-[var(--color-bg)] rounded-full transition-all duration-400 ", { 'bg-[var(--color-btn)] border border-[var(--color-bg)]': isSelected  })}>
-
-      <input type="checkbox" name="" id="check" className="absolute -z-1 hidden" value="" />
-      <label htmlFor="check" className={classNames('bg-[var(--color-bg-white)] h-4 w-4 rounded-full tansition-all duration-400 ', { 'ml-5 ': isSelected })}
-      />
+    <div className="switch-notfi">
+      <button className={`toggel-btn ${isSelected ? 'selected': ''}`} onClick={() => setIsSelected(!isSelected)}>
+        <div className="thumb"></div>
+      </button>
 
     </div>
+
   )
 }
 
--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
 import Btn from './shared/Btn';
 import aichat from '../assets/icons/sidebar/ai-chat.webp';
 import dashboard from '../assets/icons/sidebar/dashboard.webp';
-import dashboardActive from '../assets/icons/sidebar/dashboardActive.svg';
+// import dashboardActive from '../assets/icons/sidebar/dashboardActive.svg';
 import goal from '../assets/icons/sidebar/goal.webp';
 import goalActive from '../assets/icons/sidebar/goalActive.svg';
 import community from '../assets/icons/sidebar/community.webp';
@@ -13,85 +13,46 @@ import settingActive from '../assets/icons/sidebar/settingActive.svg';
 
 // use to get pages-link 
 var locaction = window.location.pathname;
-console.log(locaction);
+// console.log(locaction);
 
 const SideBar = () => {
+    const loc = useLocation()
+    console.log(loc)
     const navigate = useNavigate()
     return (
         <div className='sidebar bg-[var(--color-bg-white)] flex flex-col rounded-tl-[8px] rounded-bl-[8px]  h-full w-full'>
 
             <div className='flex flex-col justify-between h-full w-full p-3'>
-
                 <div className='sidebar-links flex flex-col gap-y-2  text-[var(--color-dark-light)]'>
-
-
-                    {/* dashboard */}
-                    {locaction == '/dashboard-page'
-                        ?
-                        <NavLink to='/dashboard-page' className='text-[var(--color-btn)] flex gap-2 shadow-md rounded-[8px] px-4 py-3'>
-                            <img src={dashboardActive} alt="" />
-                            <p className='font-medium'>  Dashboard  </p>
-                        </NavLink>
-                        :
-                        <NavLink to='/dashboard-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
-                            <img src={dashboard} alt="" />
-                            <p className='font-medium'>  Dashboard  </p>
-                        </NavLink>
-                    }
-
-
+                    <NavLink to='/dashboard-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3' >
+                        <img src={dashboard} alt="" />
+                        <p className='font-medium'>  Dashboard  </p>
+                    </NavLink>
 
                     {/* ai-chat */}
-                    <NavLink to='chat' className={` flex gap-2 items-center hover:shadow-md rounded-[8px] px-4 py-3`}>
+                    <NavLink to='conversation' className={` flex gap-2 items-center hover:shadow-md rounded-[8px] px-4 py-3`}>
                         <img src={aichat} alt="" />
                         <p className='font-medium'>  AI Health Coach  </p>
                     </NavLink>
 
-
-
                     {/* Goals */}
-                    {locaction == '/goals-page'
-                        ?
-                        <NavLink to='/goals-page' className='text-[var(--color-btn)] flex gap-2 shadow-md rounded-[8px] px-4 py-3'>
-                            <img src={goalActive} alt="" />
-                            <p className='font-medium'>  Goals  </p>
-                        </NavLink>
-                        :
-                        <NavLink to='/goals-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
-                            <img src={goal} alt="" />
-                            <p className='font-medium'>  Goals  </p>
-                        </NavLink>
-                    }
 
+                    <NavLink to='/goals-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
+                        <img src={goal} alt="" />
+                        <p className='font-medium'>  Goals  </p>
+                    </NavLink>
 
                     {/* Community */}
-                    {locaction == '/community-page'
-                        ?
-                        <NavLink to='/community-page' className='text-[var(--color-btn)] flex gap-2 shadow-md rounded-[8px] px-4 py-3'>
-                            <img src={communityActive} alt="" />
-                            <p className='font-medium'>  Community  </p>
-                        </NavLink>
-                        :
-                        <NavLink to='/community-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
-                            <img src={community} alt="" />
-                            <p className='font-medium'>  Community  </p>
-                        </NavLink>
-                    }
+                    <NavLink to='/community-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
+                        <img src={community} alt="" />
+                        <p className='font-medium'>  Community  </p>
+                    </NavLink>
 
                     {/* Settings */}
-                    {locaction == '/settings-page'
-                        ?
-                        <NavLink to='/settings-page' className='text-[var(--color-btn)] flex gap-2 shadow-md rounded-[8px] px-4 py-3'>
-                            <img src={settingActive} alt="" />
-                            <p className='font-medium'>  Settings  </p>
-                        </NavLink>
-                        :
-                        <NavLink to='/settings-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
-                            <img src={setting} alt="" />
-                            <p className='font-medium'>  Settings  </p>
-                        </NavLink>
-                    }
-
+                    <NavLink to='/settings-page' className=' flex gap-2 hover:shadow-md rounded-[8px] px-4 py-3'>
+                        <img src={setting} alt="" />
+                        <p className='font-medium'>  Settings  </p>
+                    </NavLink>
                 </div>
 
                 <div>
